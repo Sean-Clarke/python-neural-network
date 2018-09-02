f6561c2a38437eccf54c94861e05aab8003182bd
Sean A Clarke <sean.alexander.clarke@gmail.com>
2018-09-02 11:32:38 -0700

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,3 +1,154 @@
+import random
+
+class Neuron:
+    """Represents a single node in a simple feed-forward neural network supporting backpropagation"""
+    def __init__(self):
+        self.input = None
+        self.activation = None
+        self.derivative = None
+        self.error = None
+        self.effect = None
+        
+    def __repr__(self):
+        return str(self.activation)
+        
+    def get_input(self):
+        return self.input()
+        
+    def get_activation(self):
+        return self.activation
+        
+    def get_derivative(self):
+        return self.derivative
+        
+    def get_error(self):
+        return self.error
+        
+    def get_effect(self):
+        return self.effect
+        
+    def activate(self):
+        self.activation = 1.0 / (1.0 + 2.718281828459**-self.input)
+        
+    def derive(self):
+        self.derivative = self.activation * (1.0 - self.activation)
+        
+    def evaluate(self, actual):
+        self.error = 0.5 * (actual - self.activation)**2
+        self.effect = self.activation - actual
+        
+    def set_input(self, input):
+        self.input = input
+        self.activate()
+        self.derive()
+        
+    def set_activation(self, activation):
+        self.activation = activation
+        self.derive()
+        
+    def set_derivative(self, derivative):
+        self.derivative = derivative
+        
+    def set_effect(self, effect):
+        self.effect = effect
+        
+
+class Network:
+    """Represents a simple feed-forward neural network capable of weight optimization via backpropagation"""
+    def __init__(self, architecture):
+        self.layers = [[Neuron() for n in range(l)] for l in architecture]
+        self.weights = [[[random.random() for w in range(len(self.layers[l - 1]))] for h in range(len(self.layers[l]))] for l in range(1, len(self.layers))]
+        self.biases = False
+    def __repr__(self):
+        return str(self.layers[-1])
+     
+    def print_inputs(self):
+        print(str(self.layers[0]))
+     
+    def print_nodes(self):
+        print('\n'.join([str(layer) for layer in self.layers]))
+        
+    def print_weights(self):
+        print('\n'.join([str(weight) for weight in self.weights]))
+        
+    def set_inputs(self, inputs):
+        if len(inputs) != len(self.layers[0]):
+            raise ValueError("Length of given input array must equal the called network's input layer width!")
+            return False
+        for i in range(len(inputs)):
+            self.layers[0][i].set_activation(inputs[i])
+            
+    def set_weights(self, weights):
+        self.weights = weights
+        
+    def set_biases(self, biases):
+        self.biases = biases
+    
+    def feedforward(self):
+        for l in range(1, len(self.layers)):
+            for n in range(len(self.layers[l])):
+                net = sum([self.weights[l - 1][n][p] * self.layers[l - 1][p].get_activation() for p in range(len(self.layers[l - 1]))])
+                if self.biases:
+                    net += self.biases[l - 1]
+                self.layers[l][n].set_input(net)
+        
+    def backpropagate(self, key, eta, iterations=1):
+        for a in range(len(key)):
+            self.layers[-1][a].evaluate(key[a])
+        output_error = sum([n.get_error() for n in self.layers[-1] ])
+        new_weights = [[[0 for w in range(len(self.weights[m][n]))] for n in range(len(self.weights[m]))] for m in range(len(self.weights))]
+        for _ in range(iterations):
+            for l in range(len(self.layers) - 1, 0, -1):
+                for o in range(len(self.layers[l])):
+                    for i in range(len(self.layers[l - 1])):
+                        if l == len(self.layers) - 1:
+                            f = self.layers[l][o].get_activation() - key[o]
+                        else:
+                            node_effect = sum([self.weights[l][n][o] * self.layers[l + 1][n].get_effect() for n in range(len(self.layers[l + 1]))])
+                            self.layers[l][o].set_effect(node_effect)
+                            f = sum([(node_effect * (self.layers[l + 1][n].get_derivative())) * self.weights[l][n][o] for n in range(len(self.layers[l + 1]))])
+                        s = self.layers[l][o].get_derivative()
+                        t = self.layers[l - 1][i].get_activation()
+                        delta = f * s * t
+                        new_weights[l - 1][o][i] = self.weights[l - 1][o][i] - eta * delta
+            self.weights = new_weights
+        
+    def train(self, inputs, answers, eta=0.25, epoches=1):
+        if len(inputs) != len(answers):
+            raise ValueError("The number of input arrays and answer arrays must match!")
+            return False
+        for i in range(len(inputs)):
+            if len(inputs[i]) != len(self.layers[0]):
+                raise ValueError("Each input array must be the same size as the called network's input layer width!")
+                return False
+        for i in range(len(answers)):
+            if len(answers[i]) != len(self.layers[-1]):
+                raise ValueError("Each answer array must be the same size as the called network's output layer width!")
+                return False
+        if len(inputs) == 1:
+            iterations = epoches
+        else:
+            iterations = 1
+        for e in range(epoches):
+            for i in range(len(inputs)):
+                for n in range(len(self.layers[0])):
+                    self.layers[0][n].set_activation(inputs[i][n])
+                self.feedforward()
+                for _ in range(iterations):
+                    self.backpropagate(answers[i], eta, iterations=iterations)
+    
+    def test(self, inputs):
+        for i in range(len(inputs)):
+            if len(inputs[i]) != len(self.layers[0]):
+                print("Skipping input array " + str(i + 1) +  " as the number of inputs given does not match the the number of nodes in the called network's input layer!")
+                continue
+            for n in range(len(self.layers[0])):
+                self.layers[0][n].set_activation(inputs[i][n])
+            self.feedforward()
+            self.print_inputs()
+            print(str(self.layers[-1]))
+            
+"""
 import numpy
 import matplotlib
 import random
@@ -12,9 +163,7 @@ def softmax(values):
     return [numpy.exp(v)/numpy.sum(numpy.exp(values)) for v in values]
 
 class Network:
-    """Represents a neural network"""
     def __init__(self, architecture):
-        """Class initialization special method, architecture is supplied as follows: [inputs, *width_of_hidden_layer_0...*width_of_hidden_layer_n, outputs]"""
         self.depth = len(architecture) - 1
         self.layers = [[]]
         for i in range(architecture[0]):
@@ -34,14 +183,12 @@ class Network:
             self.layers[-1].append({'node':'o' + str(o), 'weights':weights, 'value':None})
             
     def __repr__(self):
-        """Class representation special method"""
         architecture = []
         for l in self.layers:
             architecture.append(len(l))
         return str(architecture)
                 
     def add_layer(self, width, list_of_weights, index=-1):
-        """Adds a new hidden layer with width nodes to the network, in given or default -1 index, with weights mapped from list_of_weights"""
         if index >= len(self.layers):
             raise ValueError('You cannot add a new hidden layer after the output layer')
         if index == 0 or index + len(self.layers) <= 0:
@@ -64,14 +211,12 @@ class Network:
         self.layers.insert(index, nl)
 
     def randomize(self):
-        """Randomizes the networks weights"""
         for l in range(1, len(self.layers)):
             for n in range(len(self.layers[l])):
                 for w in range(len(self.layers[l][n]['weights'])):
                     self.layers[l][n]['weights'][w] = numpy.random.rand()
         
     def analyze(self, input):
-        """Analzyes given list or matrix of input data, returning network outputs"""
         if len(input) != len(self.layers[0]):
             raise ValueError('number of values in input must match the network\'s input layer width (ie. number of nodes in Network.layers[0])')
         for i in range(len(input)):
@@ -91,7 +236,6 @@ class Network:
         return output
         
     def backpropogate(self, output, answer, learning_rate):
-        """Adjusts weights based on comparison between output and answer"""
         loss = 0.5 * (answer - output)**2
         for l in range(len(self.layers[1:])):
             for n in range(len(self.layers[l])):
@@ -100,11 +244,9 @@ class Network:
                     self.layers[l][n]['weights'][w] += delta
         
     def train(self, data):
-        """Trains this network with given dataset/answer key, adjusting/optimizing weights"""
         pass
             
     def test(self, data):
-        """Tests this network on given dataset/answer key"""
         for d in data:
             pass
         
@@ -113,3 +255,4 @@ iris_dataset = [[5.1, 3.5, 1.4, 0.2, 'I. setosa'],[4.9, 3.0, 1.4, 0.2, 'I. setos
 random.shuffle(iris_dataset)
 iris_training_set = iris_dataset[:int(len(iris_dataset) * 0.8)]
 iris_test_set = iris_dataset[int(len(iris_dataset) * 0.8):]
+"""
